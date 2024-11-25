a2d32db0c670d08499f4aa08549e29b25d06a5fe
pascalcyusa <shyakaadolf@gmail.com>
2024-11-25 21:03:28 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+# Set up GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+GPIO.output(16, GPIO.LOW)  # Ensure the LED is off initially
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pinon')
+def pin_on():
+    GPIO.output(16, GPIO.HIGH)  # Turn the LED on
+    return 'I turned on the pin.'
+
+@app.route('/pinoff')
+def pin_off():
+    GPIO.output(16, GPIO.LOW)  # Turn the LED off
+    return 'I turned off the pin.'
+
+@app.teardown_appcontext
+def cleanup_gpio(exception):
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
