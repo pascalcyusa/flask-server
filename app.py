c2e323966ad1234bfe0707c8266cfcfce3f9a472
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2024-11-25 22:31:19 -0500

    Change server port from 5000 to 5001 in app.py

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ def handle_command(command):
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5001, debug=True)
     finally:
         GPIO.cleanup()
