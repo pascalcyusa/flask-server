61c44207fd8b64d1a2f95bfecb9be9df047ad65f
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2024-11-25 19:22:47 -0500

    Set up GPIO for LED control and update pin endpoints

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+LED_PIN = 16
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 
 @app.route('/')
@@ -10,15 +17,18 @@ def index():
 
 @app.route('/pinon')
 def pin_on():
-    # Simulate turning the pin on
+    GPIO.output(LED_PIN, GPIO.HIGH)
     return "Pin is turned ON"
 
 
 @app.route('/pinoff')
 def pin_off():
-    # Simulate turning the pin off
+    GPIO.output(LED_PIN, GPIO.LOW)
     return "Pin is turned OFF"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    finally:
+        GPIO.cleanup()
